7f4d9368e376b451c1ec2283a22504c81460e640
sena1771 <enginsena00@gmail.com>
2022-02-28 12:45:19 +0300

    simple food controller

---
+++ GetFood.Api/Controllers/FoodController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GetFood.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        [HttpGet]
+        public string GetFood()
+        {
+            return null;
+        }
+    }
+}
